d62f7e4404a9f77e6dca0ce7e6a3d6edea34ac1b
Jack Rosenberg <jackrosenberg99@gmail.com>
2020-06-23 14:19:15 -0700

    Updated Homepage

--- js/screens/example/HomeScreen.js
+++ js/screens/example/HomeScreen.js
@@ -1,16 +1,8 @@
 /* eslint-disable global-require */
 import React from 'react';
-import { View, Image, StyleSheet, Text, Alert } from 'react-native';
+import { StyleSheet, Text, Alert } from 'react-native';
 import PropTypes from 'prop-types';
 import { ScrollView, TouchableOpacity } from 'react-native-gesture-handler';
-
-const ICON_IMAGES = [
-  require('../../../assets/fire-house-icon.png'),
-  require('../../../assets/fire-work-icon.png'),
-  require('../../../assets/PT-fire-icon.png'),
-  require('../../../assets/driving-fire-icon.png'),
-  require('../../../assets/street-fire-icon.jpg'),
-];
 
 function EnvironmentCard({ location, description, onPressFxn }) {
   return (
@@ -31,31 +23,26 @@ export default function HomePage() {
       <EnvironmentCard
         location="Home"
         description="For use in your home"
-        // image={ICON_IMAGES[0]}
         onPressFxn={() => Alert.alert('Linking Placeholder')}
       />
       <EnvironmentCard
         location="Work"
         description="For use at work"
-        // image={ICON_IMAGES[1]}
         onPressFxn={() => Alert.alert('work clicked')}
       />
       <EnvironmentCard
         location="Public Transit"
         description="For use on Public Transit"
-        // image={ICON_IMAGES[2]}
         onPressFxn={() => Alert.alert('public transport clicked')}
       />
       <EnvironmentCard
         location="Driving"
         description="For use when pulled over"
-        // image={ICON_IMAGES[3]}
         onPressFxn={() => Alert.alert('driving clicked')}
       />
       <EnvironmentCard
         location="Street"
         description="For use on the street"
-        // image={ICON_IMAGES[4]}
         onPressFxn={() => Alert.alert('street clicked')}
       />
     </ScrollView>
@@ -65,16 +52,10 @@ export default function HomePage() {
 EnvironmentCard.propTypes = {
   location: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
-  // image: Image.propTypes.source.isRequired,
   onPressFxn: PropTypes.func.isRequired,
 };
 
 const styles = StyleSheet.create({
-  image: {
-    height: 75,
-    width: 75,
-    alignContent: 'flex-end',
-  },
   KYRText: {
     fontSize: 30,
     fontWeight: 'bold',
@@ -89,8 +70,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#FDECDF',
-    // alignItems: 'center',
-    // justifyContent: 'center',
   },
 
   envCard: {
