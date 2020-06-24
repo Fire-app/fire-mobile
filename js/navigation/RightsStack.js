a7c5ccf7e7b96e98e01d08d40107ec599e566fa8
Ki Wan <kiwan.sim@gmail.com>
2020-06-24 10:08:07 -0400

    Change header text based on language setting

--- js/navigation/RightsStack.js
+++ js/navigation/RightsStack.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
+import { useTranslation } from 'react-i18next';
 
 import {
   DrivingScreen,
@@ -16,41 +17,56 @@ import routes from './routes';
 
 const Stack = createStackNavigator();
 
-const RightsStack = () => (
-  <Stack.Navigator
-    name={routes.rightsStack}
-    initialRouteName={routes.rights.overviewList}
-  >
-    <Stack.Screen
-      name={routes.rights.overviewList}
-      component={knowYourRights}
-    />
-    <Stack.Screen
-      name={routes.rights.cases.insideHome}
-      component={InsideHomeScreen}
-    />
-    <Stack.Screen
-      name={routes.rights.cases.outsideHome}
-      component={OutsideHomeScreen}
-    />
-    <Stack.Screen
-      name={routes.rights.cases.homeArrest}
-      component={HomeArrestScreen}
-    />
-    <Stack.Screen
-      name={routes.rights.cases.driving}
-      component={DrivingScreen}
-    />
-    <Stack.Screen
-      name={routes.rights.cases.publicTransport}
-      component={PublicTransportScreen}
-    />
-    <Stack.Screen
-      name={routes.rights.cases.street}
-      component={OnStreetScreen}
-    />
-    <Stack.Screen name={routes.rights.cases.work} component={WorkScreen} />
-  </Stack.Navigator>
-);
+const RightsStack = () => {
+  const { t } = useTranslation();
+
+  return (
+    <Stack.Navigator
+      name={routes.rightsStack}
+      initialRouteName={routes.rights.overviewList}
+    >
+      <Stack.Screen
+        name={routes.rights.overviewList}
+        component={knowYourRights}
+        options={{ title: t('header_rights') }}
+      />
+      <Stack.Screen
+        name={routes.rights.cases.insideHome}
+        component={InsideHomeScreen}
+        options={{ title: t('header_inside_home') }}
+      />
+      <Stack.Screen
+        name={routes.rights.cases.outsideHome}
+        component={OutsideHomeScreen}
+        options={{ title: t('header_outside_home') }}
+      />
+      <Stack.Screen
+        name={routes.rights.cases.homeArrest}
+        component={HomeArrestScreen}
+        options={{ title: t('header_home_arrest') }}
+      />
+      <Stack.Screen
+        name={routes.rights.cases.driving}
+        component={DrivingScreen}
+        options={{ title: t('header_driving') }}
+      />
+      <Stack.Screen
+        name={routes.rights.cases.publicTransport}
+        component={PublicTransportScreen}
+        options={{ title: t('header_public_transport') }}
+      />
+      <Stack.Screen
+        name={routes.rights.cases.street}
+        component={OnStreetScreen}
+        options={{ title: t('header_street') }}
+      />
+      <Stack.Screen
+        name={routes.rights.cases.work}
+        component={WorkScreen}
+        options={{ title: t('header_work') }}
+      />
+    </Stack.Navigator>
+  );
+};
 
 export default RightsStack;
