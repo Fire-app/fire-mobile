8d41b7f1786b83009a93c6392bae013a5767c10c
Jacob Jaffe <jacobmaxjaffe@gmail.com>
2020-08-11 01:11:06 -0400

    diagnostics (#50)
    Co-authored-by: K Narangerel <31663746+knarangerel@users.noreply.github.com>

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,6 @@
-/* eslint-disable func-names */
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ['babel-preset-expo', 'module:react-native-dotenv'],
   };
 };
