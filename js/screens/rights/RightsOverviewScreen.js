9b87448dc4964ceffad99d62137b8ea272c0be80
Jacob <JacobMaxJaffe@gmail.com>
2020-09-11 13:36:15 -0400

    disclaimer

--- js/screens/rights/RightsOverviewScreen.js
+++ js/screens/rights/RightsOverviewScreen.js
@@ -1,38 +1,90 @@
-import { Image, ScrollView, StatusBar, StyleSheet, View } from 'react-native';
+import {
+  Image,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  View,
+  Text,
+} from 'react-native';
 import { useTranslation } from 'react-i18next';
 import PropTypes from 'prop-types';
 import React from 'react';
 import NavCard from '../../components/NavCard';
-import { colors } from '../../styles';
+import { colors, textStyles } from '../../styles';
 import routes from '../../navigation/routes';
+import FireIcon, { ICON_NAMES } from '../../components/FireIcon';
 
 const IMAGE = require('../../../assets/illustration1.png');
+
+const DisclaimerCard = () => {
+  const { t } = useTranslation();
+  return (
+    <View
+      style={{
+        backgroundColor: colors.border,
+        paddingHorizontal: 16,
+        paddingVertical: 10,
+      }}
+    >
+      <View
+        style={{
+          alignItems: 'center',
+          borderRadius: 3,
+          flexDirection: 'row',
+          paddingBottom: 4,
+        }}
+      >
+        <FireIcon
+          color={textStyles.h2.color}
+          name={ICON_NAMES.ALARM_LIGHT}
+          size={20}
+          style={{ paddingRight: 8 }}
+        />
+        <Text style={textStyles.h3}>{t('disclaimer_title')}</Text>
+      </View>
+      <Text style={textStyles.body2}>{t('disclaimer_body')}</Text>
+    </View>
+  );
+};
 
 export default function RightsOverviewScreen({ navigation }) {
   const { t } = useTranslation();
   return (
-    <ScrollView
-      alwaysBounceVertical={false}
-      contentContainerStyle={styles.container}
+    <View
+      style={{
+        backgroundColor: colors.backgroundColor,
+        flex: 1,
+      }}
     >
       <StatusBar barStyle="dark-content" />
-      <NavCard
-        description={t('scenarios_description')}
-        onPress={() => {
-          navigation.navigate(routes.scenarios.overviewList);
+      <ScrollView
+        alwaysBounceVertical={false}
+        contentContainerStyle={{
+          flexGrow: 1,
+          marginHorizontal: 20,
+          paddingVertical: 20,
         }}
-        title={t('scenarios_title')}
-      />
-      <View style={{ height: 8 }} />
-      <NavCard
-        description={t('videos_description')}
-        onPress={() => {
-          navigation.navigate(routes.videos.overviewList);
-        }}
-        title={t('videos_title')}
-      />
+      >
+        <NavCard
+          description={t('scenarios_description')}
+          onPress={() => {
+            navigation.navigate(routes.scenarios.overviewList);
+          }}
+          title={t('scenarios_title')}
+        />
+        <View style={{ height: 8 }} />
+        <NavCard
+          description={t('videos_description')}
+          onPress={() => {
+            navigation.navigate(routes.videos.overviewList);
+          }}
+          title={t('videos_title')}
+        />
+        <View style={{ height: 8 }} />
+        <DisclaimerCard />
+      </ScrollView>
       <View
-        style={{ flexGrow: 1, justifyContent: 'flex-end', paddingBottom: 10 }}
+        style={{ bottom: 12, position: 'absolute', width: '100%', zIndex: -1 }}
       >
         <Image
           accessibilityLabel="Illustration"
@@ -40,7 +92,7 @@ export default function RightsOverviewScreen({ navigation }) {
           style={styles.image}
         />
       </View>
-    </ScrollView>
+    </View>
   );
 }
 
@@ -51,11 +103,6 @@ RightsOverviewScreen.propTypes = {
 };
 
 const styles = StyleSheet.create({
-  container: {
-    backgroundColor: colors.backgroundColor,
-    flexGrow: 1,
-    padding: 20,
-  },
   image: {
     alignSelf: 'center',
     height: 180,
