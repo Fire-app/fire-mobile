a0b1f345dc612e929cfd7d2d450a747f83366d0c
knarangerel <khaliun.narangerel@gmail.com>
2020-09-15 19:22:24 -0400

    Build new onboarding info screen

--- js/screens/onboarding/index.js
+++ js/screens/onboarding/index.js
@@ -1,6 +1,5 @@
 export { default as LanguageScreen } from './OnboardingLanguageScreen';
 export { default as WelcomeScreen } from './WelcomeScreen';
-export { default as IntroScreen } from './IntroScreen';
-export { default as ToolkitIntroScreen } from './ToolkitIntroScreen';
+export { default as InfoScreen } from './InfoScreen';
 export { default as HotlineScreen } from './HotlineScreen';
 export { default as CompleteScreen } from './CompleteScreen';
